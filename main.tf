9cb0713857e467c1f57e9c068f38679b15886403
bermetakub <alymkulovabk@gmail.com>
2024-09-27 03:46:10 +0600

    Initial commit on task_1 branch

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "VPC-bermet" {
+    cidr_block = var.vpcCIDR 
+}
+
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.VPC-bermet.id
+    cidr_block = var.subnet1_CIDR
+}
+
+resource "aws_instance" "instance-hw" {
+    ami = data.aws_ami.AmazonLinux.id
+    instance_type = var.instance-type
+    subnet_id = aws_subnet.subnet-1.id
+}
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "VPC_ID" {
+  value = aws_vpc.VPC-bermet.id
+}
+
+output "VPC_CIDR_block" {
+    value = aws_vpc.VPC-bermet.cidr_block
+}
+
+output "subnet1_id" {
+    value = aws_subnet.subnet-1.id
+}
+
+output "instanceID" {
+    value = aws_instance.instance-hw.id
+}
