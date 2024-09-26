6d03e14814dc77a0b85f5b3a98885b70bf0a3a44
bermetakub <alymkulovabk@gmail.com>
2024-09-27 04:02:12 +0600

    Initial commit on task_1 branch

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,13 @@ output "VPC_ID" {
 }
 
 output "VPC_CIDR_block" {
-    value = aws_vpc.VPC-bermet.cidr_block
+  value = aws_vpc.VPC-bermet.cidr_block
 }
 
 output "subnet1_id" {
-    value = aws_subnet.subnet-1.id
+  value = aws_subnet.subnet-1.id
 }
 
 output "instanceID" {
-    value = aws_instance.instance-hw.id
+  value = aws_instance.instance-hw.id
 }
--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "region" {
   description = "Region where resources will be deployed"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpcCIDR" {
-    description = "CIDR block for VPC"
-    type = string
+  description = "CIDR block for VPC"
+  type        = string
 }
 
 variable "subnet1_CIDR" {
-    description = "CIDR block for first subnet"
-    type = string
+  description = "CIDR block for first subnet"
+  type        = string
 }
 
 variable "subnet2_CIDR" {
-    description = "CIDR block for second subnet"
-    type = string
+  description = "CIDR block for second subnet"
+  type        = string
 }
 
 variable "instance-type" {
-    description = "Type of the instance"
-    type = string
-    default = "t2.micro"
+  description = "Type of the instance"
+  type        = string
+  default     = "t2.micro"
 }
