9b5484c80c8695ff41e0272fc03a2bb0e1e73059
bermetakub <alymkulovabk@gmail.com>
2024-09-30 04:52:19 +0600

    updating workflow file

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "AmazonLinux" {
   most_recent = true
   owners      = ["137112412989"]
   filter {
-    name   = "name"
+    name   = "test"
     values = ["al2023-ami-2023.4.20240401.1-kernel-6.1-x86_64"]
   }
 }
