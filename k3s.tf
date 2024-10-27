a4d57a070e9422b90f8ff1c449d3a3fd725a6d80
bermetakub <alymkulovabk@gmail.com>
2024-10-28 01:03:39 +0600

    Start

--- k3s.tf
+++ k3s.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "k3s_master" {
   ami             = "ami-07caf09b362be10b8"
-  instance_type   = "t2.micro"
+  instance_type   = "t2.small"
   subnet_id       = aws_subnet.public_subnet[sort(keys(aws_subnet.public_subnet))[0]].id
   security_groups = [aws_security_group.public_sg.id]
   key_name        = "gh"
@@ -13,7 +13,7 @@ resource "aws_instance" "k3s_master" {
 # EC2 Instance for k3s agent (worker node)
 resource "aws_instance" "k3s_agent" {
   ami             = "ami-07caf09b362be10b8"
-  instance_type   = "t2.micro"
+  instance_type   = "t2.small"
   subnet_id       = aws_subnet.public_subnet[sort(keys(aws_subnet.public_subnet))[0]].id
   security_groups = [aws_security_group.public_sg.id]
   key_name        = "gh"
